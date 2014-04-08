4869c992f1c34d21787b56385d304b835dd469f2
Alex West <alexanderwwest@gmail.com>
2014-04-08 10:42:34 -0700

    Passing Spec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,28 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+  
+  var elements = [];
+  var classNames = className.split(" ");
+
+  var getElements = function(element){
+  	var classes = element.classList;
+
+  	for(var i=0; i<classNames.length; i++){
+      if(element instanceof HTMLElement){
+  		  if(classes.contains(classNames[i])){
+  		  	elements.push(element);
+  		  }
+      }
+  	}
+  	if(element.hasChildNodes()){
+  		for(var child = 0; child<element.childNodes.length; child++){
+  			getElements(element.childNodes[child]);
+  		}
+  	}
+  }
+
+  getElements(document.body);
+
+  return elements;
 };
